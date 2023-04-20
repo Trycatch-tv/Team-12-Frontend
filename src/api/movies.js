26ce0e025504e899b00667d4b1b92689309aa36f
jaimesanchezgalvisacct <jaime.sanchez@acctglobal>
2023-04-21 01:18:32 +0200

    feat: conected back and add search and filter features

--- src/api/movies.js
+++ src/api/movies.js
@@ -1,34 +1,62 @@
-import data from "../data/catalogo.json";
-
-export const searchMovies = async ({ search }) => {
-
-  if (!search || search === "") {
-    return data.map((movie) => ({
-      titulo: movie.titulo,
-      genero: movie.genero,
-      año: movie.año,
-      director: movie.director,
-      calificacion: movie.calificacion,
-      reparto: movie.reparto,
-      banner: movie.banner,
-      sinopsis: movie.sinopsis,
-    }));
+// función que obtiene los resultados de la API y los almacena en caché
+const fetchMovies = async () => {
+  try {
+    const response = await fetch(
+      `https://api.allorigins.win/get?url=${encodeURIComponent(
+        "http://52.202.2.211/api/v1/films"
+      )}`
+    );
+    if (!response.ok) {
+      throw new Error("Network response was not ok.");
+    }
+    const apiData = await response.json();
+    const movies = apiData.contents;
+    const { items } = JSON.parse(movies);
+    localStorage.setItem("moviesCache", JSON.stringify(items)); // almacenar los resultados en caché
+  } catch (error) {
+    throw new Error("Error al obtener los resultados de la API");
   }
-
-  const filteredMovies = data.filter((movie) =>
-    movie.titulo.toLowerCase().includes(search.toLowerCase())
-  );
-
-  console.log("Filtered movies:", filteredMovies);
-
-  return filteredMovies.map((movie) => ({
-    titulo: movie.titulo,
-    genero: movie.genero,
-    año: movie.año,
-    director: movie.director,
-    calificacion: movie.calificacion,
-    reparto: movie.reparto,
-    banner: movie.banner,
-    sinopsis: movie.sinopsis,
-  }));
 };
+
+// función que busca los resultados en la caché local
+export const searchMovies = async ({ search }) => {
+  if (!search || search === "") {
+    try {
+      const movies = JSON.parse(localStorage.getItem("moviesCache")); // buscar los resultados en la caché local
+      return movies.map((movie) => ({
+        titulo: movie.title,
+        genero: movie.gender.name,
+        año: movie.release_date,
+        director: movie.director,
+        calificacion: movie.calificacion,
+        reparto: movie.reparto,
+        banner: movie.poster_url ? movie.poster_url : "../assets/cinema.jpg",
+        sinopsis: movie.sinopsis,
+      }));
+    } catch (error) {
+      throw new Error("Error en la búsqueda");
+    }
+  } else {
+    try {
+      const movies = JSON.parse(localStorage.getItem("moviesCache")); // buscar los resultados en la caché local
+      const filteredMovies = movies.filter((movie) =>
+        movie.title.toLowerCase().includes(search.toLowerCase())
+      );
+      return filteredMovies.map((movie) => ({
+        titulo: movie.title,
+        genero: movie.gender.name,
+        año: movie.release_date,
+        director: movie.director,
+        calificacion: movie.calificacion,
+        reparto: movie.reparto,
+        banner: movie.poster_url ? movie.poster_url : "../assets/cinema.jpg",
+        sinopsis: movie.sinopsis,
+      }));
+    } catch (error) {
+      throw new Error("Error en la búsqueda");
+    }
+  }
+};
+
+// función que se ejecuta al inicio de la aplicación para obtener los resultados de la API y almacenarlos en caché
+fetchMovies();
--- src/components/banners/bannerMovie.jsx
+++ src/components/banners/bannerMovie.jsx
@@ -1,17 +1,58 @@
 import "./bannerMovie.css";
+import defaultBannerUrl from "../../assets/cinema.jpg";
+import { useState, useEffect } from "react";
 
 export const BannersMovie = ({ movies }) => {
   const hasMovies = movies?.length > 0;
+  const [bannerUrls, setBannerUrls] = useState([]);
+  const [loaded, setLoaded] = useState(false);
+
+  const checkImage = (url, index) => {
+    return new Promise((resolve, reject) => {
+      const img = new Image();
+      img.onload = () => {
+        setBannerUrls((prevBannerUrls) => {
+          const newBannerUrls = [...prevBannerUrls];
+          newBannerUrls[index] = url;
+          return newBannerUrls;
+        });
+        resolve();
+      };
+      img.onerror = () => resolve();
+      img.src = url;
+    });
+  };
+
+  useEffect(() => {
+    if (hasMovies) {
+      const promises = movies.map((movie, index) =>
+        checkImage(movie.banner, index)
+      );
+      Promise.all(promises).then(() => setLoaded(true));
+    }
+  }, [hasMovies, movies]);
 
   return (
     <>
       {hasMovies ? (
         <ul className="movies">
-          {movies?.map((movie, index) => (
+          {movies.map((movie, index) => (
             <li className="movie" key={index}>
               <h4>{movie.titulo}</h4>
               <p>{movie.año}</p>
-              <img className="banner" src={movie.banner} alt={movie.Title} />
+              {loaded && bannerUrls[index] ? (
+                <img
+                  className="banner 1"
+                  src={bannerUrls[index]}
+                  alt={movie.titulo}
+                />
+              ) : (
+                <img
+                  className="banner 2"
+                  src={defaultBannerUrl}
+                  alt="Imagen por defecto"
+                />
+              )}
             </li>
           ))}
         </ul>
