0fbc0d57e38fc8da4393882a54220a0f573b8652
jaimesanchezgalvisacct <jaime.sanchez@acctglobal>
2023-04-26 02:53:01 +0200

    feat: add aboud us page

--- src/pages/AboudUs.jsx
+++ src/pages/AboudUs.jsx
@@ -1,9 +1,36 @@
 import React from "react";
+import { DevelopCards } from "../components/DevelopCards";
+import data from "../data/data.json";
+import { Container, Grid, Row } from "@nextui-org/react";
+
 
 export const AboudUs = () => {
   return (
-    <div>
-      <h1>AboudUs.</h1>
-    </div>
+    <Grid.Container
+      gap={2}
+      alignItems="center"
+      justify="center"
+      css={{
+        padding: "1rem",
+        width: "100%",
+        maxW: "1200px",
+        margin: "0 auto",
+      }}
+    >
+      {data.map((info, index) => (
+        <Grid
+          sm={4}
+          md={3}
+          css={{
+            padding: "0.5rem",
+            width: "100%",
+            margin: "0 auto",
+            maxW: "250px",
+          }}
+        >
+          <DevelopCards key={index} {...info} />
+        </Grid>
+      ))}
+    </Grid.Container>
   );
 };
